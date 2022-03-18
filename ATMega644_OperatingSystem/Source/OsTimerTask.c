5a9f1bc16fda2f48a75a3efc9b6bff1758323614
LeonAblinger <leon.ablinger@outlook.at>
2022-03-18 09:33:15 +0100

    Added functions for timer task

--- ATMega644_OperatingSystem/Source/OsTimerTask.c
+++ ATMega644_OperatingSystem/Source/OsTimerTask.c
@@ -4,3 +4,19 @@
  Created: 09/03/2022
  Author:  Leon Ablinger / AblL
  *******************************************************************/
+
+#include <string.h>
+#include <stdlib.h>
+#include "HTLStddef.h"
+#include "OsTimerTask.h"
+
+OsTimerTaskInit(
+	unsigned long aCpuClk,
+	unsigned long aTimerInterval
+	);
+	
+OsTimerTaskAddTask();
+OsTimerTaskRemoveTask();
+	
+OsTimerTaskStart();
+OsTimerTaskStop();
